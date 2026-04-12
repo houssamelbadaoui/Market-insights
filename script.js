2b4a0d0e403eeee33108c9af3df73d73263764db
houssamelbadaoui <houssamelbadaoui5@email.com>
2026-04-12 20:55:46 +0200

    Add function fetchCrypto from API and helper function renderCryptoCard

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ $(document).ready(function () {
     );
 
     // call the correct API based on type
+    if (type === "crypto") {
+      fetchCrypto(query);
+    }
   });
 
   // helper function to update UI for type selected
@@ -50,5 +53,46 @@ $(document).ready(function () {
 
     // update empty state
     $(".result-container").html(`<p class="empty-state">${emptyText}</p>`);
+  }
+
+  // function to fetch crypro
+  function fetchCrypto(query) {
+    // build URL
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${query}&vs_currencies=usd&include_24hr_change=true`;
+
+    // show loading
+    $(".result-container").html(`<p class="empty-state">Loading...</p>`);
+    // call $.getJSON
+    $.getJSON(url)
+      .done(function (data) {
+        // if invalid or not found
+        if (!data[query]) {
+          $(".result-container").html(
+            `<p class="empty-state">Crypto not found...</p>`,
+          );
+        }
+        // extract the values
+        const price = data[query].usd;
+        const change = data[query].usd_24h_change;
+        // on sucess => render card
+        renderCryptoCard(query, price, change);
+      })
+      // on error => show error message
+      .fail(function () {
+        // error
+        console.log("API error.");
+      });
+  }
+
+  // helper function to render crypto card
+  function renderCryptoCard(name, price, change) {
+    const card = `
+    <div class="card">
+      <h2>${name}</h2>
+      <p>Price: $${price}</p>
+      <p>24h Change: ${change.toFixed(2)}%</p>
+    </div>
+  `;
+    $(".result-container").html(card);
   }
 });
